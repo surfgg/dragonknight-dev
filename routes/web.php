6bff941dcee08a31c33368c20d9fc5245450d1a4
splashsky <skylearj@gmail.com>
2020-08-26 08:12:00 -0500

    Add WelcomeController, update route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Pages\WelcomeController@welcome')->name('welcome');

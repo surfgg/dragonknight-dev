040e547e3821f10efefe16a8c76462ec083df86c
splashsky <skylearj@gmail.com>
2020-08-26 12:34:25 -0500

    Install fonts, begin design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 module.exports = {
     purge: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'discord': {
+                    50: '#F6F8FF',
+                    100: '#EDF2FF',
+                    200: '#D3DEFF',
+                    300: '#B8CBFF',
+                    400: '#83A3FF',
+                    500: '#4E7CFF',
+                    600: '#4670E6',
+                    700: '#2F4A99',
+                    800: '#233873',
+                    900: '#17254D',
+                },
+
+                'bg': {
+                    100: '#ffffff',
+                    200: '#F3F4F5',
+                    300: '#dfe0e1',
+                    400: '#cbcccd',
+                    500: '#b7b8b9',
+                    600: '#a3a4a5',
+                    700: '#8f9091',
+                    800: '#7b7c7d',
+                    900: '#676869',
+                    1000: '#535455'
+                },
+            },
+
+            fontFamily: {
+                'display': ['Vollkorn', 'Cambria', 'Hoefler Text', 'Utopia', 'Liberation Serif', 'Nimbus Roman No9 L Regular', 'Times', 'Times New Roman', 'serif'],
+                'body': ['Montserrat', 'Frutiger', 'Frutiger Linotype', 'Univers', 'Calibri', 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'Myriad', 'DejaVu Sans Condensed', 'Liberation Sans', 'Nimbus Sans L', 'Tahoma', 'Geneva', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif']
+            },
+        },
     },
     variants: {},
     plugins: [],

3f0b4f393d2f56a28b389a2591b71106aefa1f45
splashsky <skylearj@gmail.com>
2020-08-26 19:23:11 -0500

    Increase size of mobile nav icons

--- resources/views/components/nav/mobile-nav.blade.php
+++ resources/views/components/nav/mobile-nav.blade.php
@@ -1,8 +1,8 @@
 <nav id="mobile-nav" class="fixed z-50 w-full" x-data="{ drawer: false }" @click.away="drawer = false">
     <div class="flex items-center justify-between w-full px-4 py-2 text-white" style="background-image: url('{{ asset('css/img/darktopo.jpg') }}');">
-        <h1 class="text-base"><i class="fas fa-fw fa-dragon"></i></h1>
+        <h1 class="text-xl"><i class="fas fa-fw fa-dragon"></i></h1>
 
-        <i class="far fa-fw fa-bars" :class="drawer ? 'fa-times' : 'fa-bars'" @click="drawer = !drawer"></i>
+        <i class="text-xl far fa-fw fa-bars" :class="drawer ? 'fa-times' : 'fa-bars'" @click="drawer = !drawer"></i>
     </div>
 
     <div x-cloak x-show="drawer" class="absolute left-0 w-full text-white bg-gray-800" style="top: 100%;">
